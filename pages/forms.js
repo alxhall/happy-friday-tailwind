d2689e61a5c8f2295edcec0217709f77319736e3
Alexander Hall <alexander.hall@spinit.se>
2020-11-23 11:33:41 +0100

    Fix property names for React

--- pages/forms.js
+++ pages/forms.js
@@ -20,7 +20,7 @@ function FormsPage() {
                 <div className="grid grid-cols-3 gap-6">
                   <div className="col-span-3 sm:col-span-2">
                     <label
-                      for="company_website"
+                      htmlFor="company_website"
                       className="block text-sm font-medium text-gray-700"
                     >
                       Website
@@ -41,7 +41,7 @@ function FormsPage() {
 
                 <div>
                   <label
-                    for="about"
+                    htmlFor="about"
                     className="block text-sm font-medium text-gray-700"
                   >
                     About
@@ -97,9 +97,9 @@ function FormsPage() {
                       >
                         <path
                           d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                       </svg>
                       <p className="text-sm text-gray-600">
